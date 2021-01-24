79c06c4658cf88e47d07db884d71c789283fa0c9
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-01-24 16:25:59 +0000

    Add Player Character with Art & Animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+
+    private void FixedUpdate()
+    {
+        GetComponent<PlayerMover>().Move(moveInput);
+
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void OnJump()
+    {
+        GetComponent<PlayerMover>().Jump();
+    }
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float flySpeed = 5f;
+    [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float floatHeight = 1f;
+    [SerializeField] float liftForce = 10f;
+    [SerializeField] float damping = 1f;
+
+    private Rigidbody2D myRigidbody;
+    private BoxCollider2D myFeet;
+    private Animator animator;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        Fall();
+        FlipSprite();
+    }
+
+    public void Move(Vector2 moveThrottle)
+    {
+        float moveSpeedX = 0f;
+        bool playerIsTouchingGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        if (playerIsTouchingGround)
+        {
+            animator.SetFloat("MoveBlend", Mathf.Abs(myRigidbody.velocity.x));
+            moveSpeedX = moveSpeed * Time.deltaTime * moveThrottle.x;
+        }
+        else
+        {
+            animator.SetFloat("MoveBlend", 0);
+            moveSpeedX = flySpeed * Time.deltaTime * moveThrottle.x;
+        }
+
+        Vector2 moveForce = new Vector2(moveSpeedX, 0f);
+        myRigidbody.velocity += moveForce;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = (Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon);
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(myRigidbody.velocity.x), 1f, 1f);
+        }
+    }
+
+
+    public void Jump()
+    {
+        bool playerIsTouchingGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        if (!playerIsTouchingGround) { return; }
+
+        float currentJumpSpeed = jumpSpeed * 100f;
+        Vector2 jumpForce = new Vector2(0f, currentJumpSpeed);
+        myRigidbody.AddForce(jumpForce);
+
+        animator.SetTrigger("Jump");
+    }
+
+    private void Fall()
+    {
+        bool playerHasDownwardSpeed = (myRigidbody.velocity.y < Mathf.Epsilon);
+        bool playerIsTouchingGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        if (!playerHasDownwardSpeed) { return; }
+
+        RaycastHit2D hit = Physics2D.Raycast(myRigidbody.position, -Vector2.up);
+
+        if (hit.collider != null)
+        {
+            float distance = Mathf.Abs(hit.point.y - myRigidbody.position.y);
+            float heightError = floatHeight - distance;
+            float endLineY = myRigidbody.position.y - heightError;
+            Vector2 endLinePosition = new Vector2(myRigidbody.position.x, endLineY);
+            Debug.DrawLine(myRigidbody.position, endLinePosition);
+
+            float force = liftForce * heightError - myRigidbody.velocity.y * damping;
+
+            myRigidbody.AddForce(Vector3.up * force);
+        }
+
+        animator.SetBool("isFalling", !playerIsTouchingGround);
+    }
+}
